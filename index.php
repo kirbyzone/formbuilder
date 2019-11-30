af8215fb6b6f703c3a134cc6febf8d4259fe982c
Igor Couto <igor@cre8iv.click>
2019-11-30 11:34:37 +1100

    implement single-line text field & textarea UI
    Signed-off-by: Igor Couto <igor@cre8iv.click>

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 
 Kirby::plugin('cre8ivclick/formbuilder', [
     'blueprints' => [
-        'formbuilder' => __DIR__ . '/blueprints/formbuilder.yml'
+        'formbuilder' => __DIR__ . '/blueprints/formbuilder.yml',
+        'formbuilder/fields/name' => __DIR__ . '/blueprints/fields/name.yml',
+        'formbuilder/fields/class' => __DIR__ . '/blueprints/fields/class.yml',
+        'formbuilder/fields/req' => __DIR__ . '/blueprints/fields/req.yml',
+        'formbuilder/fields/label' => __DIR__ . '/blueprints/fields/label.yml',
+        'formbuilder/fb_text' => __DIR__ . '/blueprints/blocks/fb_text.yml',
+        'formbuilder/fb_textarea' => __DIR__ . '/blueprints/blocks/fb_textarea.yml'
     ],
     'snippets' => [
         'formbuilder' => __DIR__ . '/snippets/formbuilder.php',
@@ -14,6 +20,8 @@ Kirby::plugin('cre8ivclick/formbuilder', [
         'formbuilder/select' => __DIR__ . '/snippets/fields/select.php',
         'formbuilder/radio' => __DIR__ . '/snippets/fields/radio.php',
         'formbuilder/hidden' => __DIR__ . '/snippets/fields/hidden.php',
-        'formbuilder/honeypot' => __DIR__ . '/snippets/fields/honeypot.php'
+        'formbuilder/honeypot' => __DIR__ . '/snippets/fields/honeypot.php',
+        'formbuilder/text_preview' => __DIR__ . '/snippets/previews/text.php',
+        'formbuilder/textarea_preview' => __DIR__ . '/snippets/previews/textarea.php'
     ]
 ]);
